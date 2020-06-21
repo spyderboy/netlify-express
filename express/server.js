b1a94884bda540dfebfd9a34e19a66e7268ba866
Jose Antonio Licon <jal321@pitt.edu>
2020-06-21 14:00:37 -0400

    fixed Express typo

--- express/server.js
+++ express/server.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const stream = require("stream");
 const encoding = require("encoding");
 var fs = require("fs");
-var app = Express();
+var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
